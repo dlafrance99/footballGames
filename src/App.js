5b70b18c44fb58ec54ac79d5944b8943ccda0ea3
dlafrance99 <lafrance.devon@gmail.com>
2023-08-31 15:21:09 -0600

    added password
    Updated Author

--- src/App.js
+++ src/App.js
@@ -8,10 +8,59 @@ import Footbordle from './Games/Footbordle';
 import NFLMatch from './Games/NFLMatch'
 
 const App = () => {
+  const [LoggedIn, setLoggedIn] = useState(false)
+  const [Password, setPassword] = useState('')
+  const [ErrorMessage, setErrorMessage] = useState('')
+
+  const handlePassword = () => {
+    if (Password.toLowerCase().trim() === 'respectthecommish') {
+      setLoggedIn(true)
+    } else {
+      setErrorMessage('Wrong Password')
+    }
+  }
 
   return (
     <Grid className='Wrapper'>
-      <NFLMatch />
+      {
+        LoggedIn
+          ?
+          <NFLMatch />
+          :
+          <>
+            <Row style={{ marginTop: '30vh' }}>
+              <h1 className='Guesses' style={{ color: 'white' }}>
+                Password
+              </h1>
+              {
+                ErrorMessage
+                  ?
+                  <h3 className='Guesses' style={{ color: 'red' }}>
+                    {ErrorMessage}
+                  </h3>
+                  :
+                  null
+              }
+              <Col sm={24} className='Guesses' >
+                <Input
+                  style={{ color: 'black', width: '50%', backgroundColor: 'white', border: '1px solid white', fontSize: '50px' }}
+                  onPressEnter={() => handlePassword()}
+                  value={Password}
+                  onChange={(value) => setPassword(value.toUpperCase())}
+                />
+
+                <ButtonToolbar style={{ width: '50%', margin: 'auto', marginTop: '15px' }}>
+                  <Button
+                    style={{ fontSize: '25px', padding: '10px 15px 10px 15px', backgroundColor: 'transparent', border: '2px solid green', color: 'green', borderRadius: '5px' }}
+                    onClick={() => handlePassword()}
+                  >
+                    Enter
+                  </Button>
+                </ButtonToolbar>
+              </Col>
+            </Row>
+          </>
+      }
     </Grid>
   )
 };
